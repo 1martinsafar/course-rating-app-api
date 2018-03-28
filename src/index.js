b3c0a752a4e0b7a0ed34a2eac3969b384127a04b
Martin Safar <1martinsafar@gmail.com>
2018-03-28 18:30:14 +0200

    Setting up the database connection

--- src/index.js
+++ src/index.js
@@ -1,37 +1,51 @@
-'use strict';
+"use strict";
 
 // load modules
-var express = require('express');
-var morgan = require('morgan');
+const express = require("express");
+const app = express();
 
-var app = express();
+const morgan = require("morgan");
+const mongoose = require("mongoose");
+
+// MongoDB connection
+mongoose.connect("mongodb://localhost:27017/courses");
+const db = mongoose.connection;
+// MongoDB errror
+db.on("error", err => console.error("connection error:", err));
+// MongoDB ready
+db.once("open", () => console.log("db connection successful"));
 
 // set our port
-app.set('port', process.env.PORT || 5000);
+app.set("port", process.env.PORT || 5000);
 
-// morgan gives us http request logging
-app.use(morgan('dev'));
+// morgan gives us http request logging in the console
+app.use(morgan("dev"));
 
 // setup our static route to serve files from the "public" folder
-app.use('/', express.static('public'));
+app.use("/", express.static("public"));
 
 // catch 404 and forward to global error handler
-app.use(function(req, res, next) {
-  var err = new Error('File Not Found');
+app.use((req, res, next) => {
+  const err = new Error("File Not Found");
   err.status = 404;
   next(err);
 });
 
 // Express's global error handler
-app.use(function(err, req, res, next) {
+app.use((err, req, res, next) => {
   res.status(err.status || 500);
-  res.render('error', {
+  res.render("error", {
     message: err.message,
     error: {}
   });
 });
 
 // start listening on our port
-var server = app.listen(app.get('port'), function() {
-  console.log('Express server is listening on port ' + server.address().port);
+const server = app.listen(app.get("port"), () => {
+  console.log("Express server is listening on port " + server.address().port);
 });
+
+
+
+
+//
